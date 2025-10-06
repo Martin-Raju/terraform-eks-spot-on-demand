c2b9196775b225feb6affac1a4af93e35cc9d6ab
martin Raju <martin.raju313@gmail.com>
2025-10-06 14:49:21 +0530

    updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "poc-tfstate-bucket-0123456"
-    key     = "Envs/Dev/terraform.tfstate"
+    bucket  = "my-tfstate-bucket-0123456"
+    key     = "Eks/Dev/terraform.tfstate"
     region  = "us-east-2"
     encrypt = true
   }
