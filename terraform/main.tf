8c478d3d6e45456587e366018fca0dd3a701791b
martin Raju <martin.raju313@gmail.com>
2025-10-06 17:51:10 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,13 +93,13 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.3.1"
 
-  cluster_name    = module.label.id
-  cluster_version = var.kubernetes_version
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets
-  enable_irsa     = true
-
-  manage_aws_auth = true
+  name                            = module.label.id
+  version                         = var.kubernetes_version
+  subnet_ids                      = module.vpc.private_subnets
+  vpc_id                          = module.vpc.vpc_id
+  enable_irsa                     = true
+  cluster_endpoint_public_access  = false
+  cluster_endpoint_private_access = true
 
   # Disable default node groups (Karpenter will handle nodes)
   node_groups = {}
