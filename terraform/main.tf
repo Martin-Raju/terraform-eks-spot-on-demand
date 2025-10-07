44507ff6ebdf332aa06d2d5042f7904b8a7a1f17
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-10-07 11:44:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -177,7 +177,7 @@ resource "helm_release" "karpenter" {
   name             = "${module.label.id}-karpenter"
   repository       = "oci://public.ecr.aws/karpenter/karpenter"
   chart            = "karpenter"
-  version          = "v1.6.3"
+  version          = "v0.34.0"
   namespace        = "karpenter"
   create_namespace = true
 
