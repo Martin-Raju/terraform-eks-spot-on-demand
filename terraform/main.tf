333d8574bd8b3770f11d5dc428955f15aa28cf33
martin Raju <martin.raju313@gmail.com>
2025-10-09 14:46:40 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -119,9 +119,9 @@ module "eks" {
 }
 
 module "karpenter" {
-  source = "terraform-aws-modules/eks/aws//modules/karpenter"
+  source       = "terraform-aws-modules/eks/aws//modules/karpenter"
   cluster_name = module.eks.cluster_name
-  
+
   # Attach additional IAM policies to the Karpenter node IAM role
   node_iam_role_additional_policies = {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
