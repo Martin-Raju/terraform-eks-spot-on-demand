1e577e87452398326fb8648377fb11beb35e0c95
martin Raju <martin.raju313@gmail.com>
2025-10-09 14:08:52 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -119,9 +119,6 @@ module "eks" {
     enabled = false
   }
 
-  karpenter_enabled   = true
-  enable_pod_identity = true
-
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
