31daf038d0b8f86fa146153c19032901fdbf5906
martin Raju <martin.raju313@gmail.com>
2025-10-10 17:35:45 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -186,27 +186,27 @@ module "eks" {
         "nodegroup" = "application"
         "workload"  = "app"
       }
-      taints = {
-        "lifecycle" = {
-          key    = "lifecycle"
-          value  = "spot"
-          effect = "NO_SCHEDULE"
-        }
-      }
+      # taints = {
+        # "lifecycle" = {
+          # key    = "lifecycle"
+          # value  = "spot"
+          # effect = "NO_SCHEDULE"
+        # }
+      # }
     }
-    on_demand_nodes = {
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+    # on_demand_nodes = {
+      # ami_type       = "AL2023_x86_64_STANDARD"
+      # instance_types = ["t3.medium"]
+      # capacity_type  = "ON_DEMAND"
+      # min_size       = 1
+      # max_size       = 3
+      # desired_size   = 2
 
-      labels = {
-        "lifecycle" = "on-demand"
-        "nodegroup" = "system"
-        "workload"  = "system"
-      }
+      # labels = {
+        # "lifecycle" = "on-demand"
+        # "nodegroup" = "system"
+        # "workload"  = "system"
+      # }
       # taints = {
       # "lifecycle" = {
       # key    = "lifecycle"
