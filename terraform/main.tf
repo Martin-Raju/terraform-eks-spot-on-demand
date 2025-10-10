ab46039d95a0096744a393833f5044f7e8668e10
martin Raju <martin.raju313@gmail.com>
2025-10-10 18:12:39 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -243,28 +243,28 @@ module "karpenter" {
 # Karpenter Helm Release
 # -------------------------
 
-# resource "helm_release" "karpenter" {
-# depends_on          = [module.eks, module.karpenter]
-# namespace           = "kube-system"
-# name                = "karpenter"
-# repository          = "oci://public.ecr.aws/karpenter"
-# repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-# repository_password = data.aws_ecrpublic_authorization_token.token.password
-# chart               = "karpenter"
-# version             = "1.4.1"
-# wait                = true
+resource "helm_release" "karpenter" {
+  depends_on          = [module.eks, module.karpenter]
+  namespace           = "kube-system"
+  name                = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter"
+  version             = "1.4.1"
+  wait                = true
 
-# values = [
-# <<-EOT
-# serviceAccount:
-# name: ${module.karpenter.service_account}
-# settings:
-# clusterName: ${module.eks.cluster_name}
-# clusterEndpoint: ${module.eks.cluster_endpoint}
-# interruptionQueue: ${module.karpenter.queue_name}
-# EOT
-# ]
-# }
+  values = [
+    <<-EOT
+serviceAccount:
+name: ${module.karpenter.service_account}
+settings:
+clusterName: ${module.eks.cluster_name}
+clusterEndpoint: ${module.eks.cluster_endpoint}
+interruptionQueue: ${module.karpenter.queue_name}
+EOT
+  ]
+}
 
 
 # -------------------------
