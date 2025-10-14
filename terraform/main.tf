5ec740d95e627b80b4d632303a8f0ec2e1babd97
martin Raju <martin.raju313@gmail.com>
2025-10-14 16:25:35 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,6 @@ provider "kubernetes" {
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
-  depends_on = [module.eks, time_sleep.wait_for_eks]
 }
 
 provider "helm" {
