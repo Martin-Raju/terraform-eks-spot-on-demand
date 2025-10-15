7be839c086c1a12e0690750a8c33eb13be379131
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-10-15 18:58:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -204,23 +204,17 @@ resource "time_sleep" "wait_for_eks" {
 # -------------------------
 # Karpenter Helm Release
 # -------------------------
-#resource "kubernetes_namespace" "karpenter" {
-#  metadata {
-#    name = "karpenter"
-#  }
-#  depends_on = [module.eks, time_sleep.wait_for_eks]
-#  provider   = kubernetes.eks
-#}
 
 resource "helm_release" "karpenter" {
-  count = var.eks_public_access_enabled ? 1 : 0
+  count    = var.eks_public_access_enabled ? 1 : 0
+  provider = helm
   depends_on = [
     module.eks,
     module.karpenter,
     time_sleep.wait_for_eks
   ]
+  namespace           = "kube-system"
   name                = "karpenter"
-  provider            = helm
   repository          = "oci://public.ecr.aws/karpenter"
   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
   repository_password = data.aws_ecrpublic_authorization_token.token.password
@@ -228,7 +222,6 @@ resource "helm_release" "karpenter" {
   version             = "1.6.0"
   skip_crds           = false
   create_namespace    = true
-  namespace           = "karpenter"
   wait                = true
 
   values = [
