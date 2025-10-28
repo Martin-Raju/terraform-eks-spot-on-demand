0f826942ff142fe27533e65b15b8454a2f44627e
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-10-28 17:52:33 +0530

    changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-#provider "aws" {
-#  alias  = "virginia"
-#  region = "us-east-1"
-#}
+provider "aws" {
+  alias  = "virginia"
+  region = "us-east-1"
+}
 
 #provider "kubernetes" {
 
