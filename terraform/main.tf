4853c30fc7cb91f6c7794438bf7cda57dff02017
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-10-30 15:53:36 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -167,69 +167,6 @@ module "karpenter" {
 
   }
 }
-# ==========================================================
-# Karpenter Node IAM Role (for EC2 nodes it launches)
-# ==========================================================
-resource "aws_iam_role" "karpenter_node_role" {
-  name = module.eks.cluster_name-karpenter
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = { Service = "ec2.amazonaws.com" }
-        Action    = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "karpenter_worker" {
-  role       = aws_iam_role.karpenter_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "karpenter_cni" {
-  role       = aws_iam_role.karpenter_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "karpenter_ecr" {
-  role       = aws_iam_role.karpenter_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "karpenter_ssm" {
-  role       = aws_iam_role.karpenter_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-# ==========================================================
-# IAM: Allow Karpenter Controller to Pass Role + Add to InstanceProfile
-# ==========================================================
-resource "aws_iam_role_policy" "karpenter_controller_passrole" {
-  name = "AllowPassRoleForKarpenter"
-  role = module.eks.cluster_name-karpenter
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "iam:PassRole",
-          "iam:GetRole",
-          "iam:CreateInstanceProfile",
-          "iam:AddRoleToInstanceProfile",
-          "iam:RemoveRoleFromInstanceProfile",
-          "iam:DeleteInstanceProfile"
-        ]
-        Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${module.eks.cluster_name}-karpenter"
-      }
-    ]
-  })
-}
 
 # -------------------------
 # Bastion Security Group
