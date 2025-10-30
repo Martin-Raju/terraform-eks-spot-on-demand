abbb556248e28de145865459fbb21b5fa5270d66
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-10-30 15:25:56 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -210,7 +210,7 @@ resource "aws_iam_role_policy_attachment" "karpenter_ssm" {
 # ==========================================================
 resource "aws_iam_role_policy" "karpenter_controller_passrole" {
   name = "AllowPassRoleForKarpenter"
-  role = module.karpenter.irsa_name
+  role = module.karpenter.irsa_role_name
 
   policy = jsonencode({
     Version = "2012-10-17"
